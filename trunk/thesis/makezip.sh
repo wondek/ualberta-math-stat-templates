11cd935e4b12990a961c352a2572c35dc7b238a9
squidgyhead@gmail.com <squidgyhead@gmail.com@715c15d2-bd05-1f32-93dd-5d974e83eeb7>
2011-10-04 02:55:48 +0000

    fix typo in makezip.sh

--- trunk/thesis/makezip.sh
+++ trunk/thesis/makezip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip thesi.zip convertepstopdf.sh draftmode.sty hyperbreqn.sty hypercitebracket.sty localthesis.sty mathdef.sty mathesis.sty math.sty refs.bib rmp2.bst thesis.tex   
+zip thesis_template.zip convertepstopdf.sh draftmode.sty hyperbreqn.sty hypercitebracket.sty localthesis.sty mathdef.sty mathesis.sty math.sty refs.bib rmp2.bst thesis.tex   
