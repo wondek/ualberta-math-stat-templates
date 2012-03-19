d85e07b08b33e75a52de7043918b0ba03957c3a9
squidgyhead@gmail.com <squidgyhead@gmail.com@715c15d2-bd05-1f32-93dd-5d974e83eeb7>
2012-03-19 15:46:44 +0000

    improve convertepstopdf

--- trunk/thesis/convertepstopdf.sh
+++ trunk/thesis/convertepstopdf.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-for x in *.eps; 
-do 
+
+
+if [ "$1" == "" ]; then
+    for x in *.eps; 
+    do 
+	echo 
+	echo $x
+	echo eps2eps $x
+	eps2eps $x
+	echo epstopdf $x
+	epstopdf $x
+	pdfname=$(echo $x | sed 's/eps/pdf/g' )
+	echo $pdfname
+	pdfcrop $pdfname $pdfname
+    done
+else
     echo 
-    echo $x
-    echo eps2eps $x
-    eps2eps $x
-    echo epstopdf $x
-    epstopdf $x
-    pdfname=$(echo $x | sed 's/eps/pdf/g' )
+    echo $1
+    echo eps2eps $1
+    eps2eps $1
+    echo epstopdf $1
+    epstopdf $1
+    pdfname=$(echo $1 | sed 's/eps/pdf/g' )
     echo $pdfname
     pdfcrop $pdfname $pdfname
-done
+fi
