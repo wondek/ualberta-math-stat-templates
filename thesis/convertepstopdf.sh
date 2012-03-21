6073f3d8ed6d1d0fcacdbc24fc649eee70882951
squidgyhead <squidgyhead@gmail.com>
2012-03-21 15:38:41 +0000

    minor

--- thesis/convertepstopdf.sh
+++ thesis/convertepstopdf.sh
@@ -6,22 +6,18 @@ if [ "$1" == "" ]; then
     do 
 	echo 
 	echo $x
-	echo eps2eps $x
-	eps2eps $x
-	echo epstopdf $x
+	#echo eps2eps $x
+	#eps2eps $x
+	#echo epstopdf $x
 	epstopdf $x
 	pdfname=$(echo $x | sed 's/eps/pdf/g' )
-	echo $pdfname
+	#echo $pdfname
 	pdfcrop $pdfname $pdfname
     done
 else
     echo 
     echo $1
-    echo eps2eps $1
-    eps2eps $1
-    echo epstopdf $1
     epstopdf $1
     pdfname=$(echo $1 | sed 's/eps/pdf/g' )
-    echo $pdfname
     pdfcrop $pdfname $pdfname
 fi
